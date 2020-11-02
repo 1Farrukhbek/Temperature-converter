2e00ab5af19858ffc85be906ff9c0d47697e9a28
Farrukhbek <68499298+1Farrukhbek@users.noreply.github.com>
2020-11-02 16:54:07 +0500

    Add js codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,23 @@
-var euroInUzs = 12285.78;
-var yuanInUzs = 11457.51;
-var rublInUzs = 136.11;
-var liraInUzs = 1301.33;
-var funtInUzs = 13509.28;
-var dollarInUzs = 10359.88;
+var elTempForm = document.querySelector('.temp__form');
+var elTempOutput = document.querySelector('.temp__output');
 
-var currencyConverterForm = document.querySelector('#currencyConverterForm');
-
-currencyConverterForm.addEventListener('submit', function (evt) {
+elTempForm.addEventListener('submit', function (evt) {
   evt.preventDefault();
+  
+  var elTempInput = parseFloat(document.querySelector('.temp__input').value.trim(), 10);
 
-  var currencyUnit = currencyConverterForm.querySelector('#currencyUnit').value.trim();
-
-  var currencyValue = parseFloat(currencyConverterForm.querySelector('#currencyValue').value.trim(), 10);
-
-  var currencyInSum;
-
-  if (currencyUnit === "euro") {
-    currencyInSum = currencyValue * euroInUzs;
-  } else if (currencyUnit === "yuan") {
-    currencyInSum = currencyValue * yuanInUzs;
-  } else if (currencyUnit === "rubl") {
-    currencyInSum = currencyValue * rublInUzs;
-  } else if (currencyUnit === "lira") {
-    currencyInSum = currencyValue * liraInUzs;
-  } else if (currencyUnit === "funt") {
-    currencyInSum = currencyValue * funtInUzs;
-  } else if (currencyUnit === "dollar") {
-    currencyInSum = currencyValue * dollarInUzs;
+  if (isNaN = (elTempInput)) {
+    var tempResult = (elTempInput * (9/5) + 32).toFixed(1);
+    elTempOutput.value = tempResult;
   } else {
-    currencyInSum = "Bunday pul birligi topilmadi"
+    elTempOutput.value = ('Only numbers')
   }
 
-  var convertResult = document.querySelector('#currencyInSum');
-  convertResult.textContent = currencyInSum + ' sum';
-});
-
-currencyConverterForm.addEventListener('reset', function (evt) {
-  var convertResult = document.querySelector('#currencyInSum');
-  convertResult.textContent = "";
-});
+  if (elTempInput < 0) {
+    document.body.classList = ('bg-info');
+  } else if (elTempInput < 20) {
+    document.body.classList = ('bg-warning');
+  } else {
+    document.body.classList = ('bg-danger');
+  }
+})
